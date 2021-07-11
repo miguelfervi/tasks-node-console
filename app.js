344e9c4076436c0b834c19db8e91e917c8e4c22b
Miguel <miguelfervi@gmail.com>
2021-07-11 15:46:03 +0200

    Add pause

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('colors');
 
-const {inquireMenu} = require('./helpers/inquirer');
+const {inquireMenu, pausa} = require('./helpers/inquirer');
 
 console.clear();
 
@@ -12,6 +12,9 @@ const main = async() => {
   do {
     opt = await inquireMenu();
     console.log(opt);
+
+    await pausa();
+
   } while ( opt !== '0');
 }
 
